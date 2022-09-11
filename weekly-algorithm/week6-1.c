9d10dd7a85eb2d3d55ac11f65aef58102697ff48
Gridsada Raksakun <44823938+gridsada14@users.noreply.github.com>
2022-09-11 13:53:05 +0700

    Update week6-1.c

--- weekly-algorithm/week6-1.c
+++ weekly-algorithm/week6-1.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
-int main(){
-    float a, b;
-    for (int i = 0; i < 10; i++){
-        scanf("%f", &a);
-        b += a;
+int main() {
+    int lower=0,upper=0;
+    int length=0; 
+    char str[50];
+
+    scanf("%s", str);
+
+    // Find '\0' intead using strlen()
+    for (int j = 0; str[length] != '\0'; ++j)
+        length++;
+
+    // Find upper lower without string.h
+    for (int i = 0; i < length; i++)
+    {
+        if(str[i] >= 'a' && str[i] <= 'z'){
+            lower++;
+        }else if(str[i] >= 'A' && str[i] <= 'Z'){
+            upper++;
+        }
     }
-    printf("%.2f", b/10);
-    return 0; 
+
+    printf("-> %d Upper case\n", upper);
+    printf("-> %d Lower case", lower);
+    
+    return 0;
 }
+
